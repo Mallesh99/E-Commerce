aa2388d4d850c9c27c665a2757cc543546b923c9
mallesh <mallesh@wielabs.com>
2024-09-04 17:38:24 +0530

    for cors

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,16 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-const corsOptions = {
-  origin: "*",
-  credentials: true,
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+  );
+  next();
+});
 
 app.use(express.json());
 app.use("/images", express.static("images"));
